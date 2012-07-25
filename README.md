4d1b2955cb030322bb0efa50a4ac248ee10c238b
Igor Sokolov <igor.sokolov@gmail.com>
2012-07-25 12:17:48 +0300

    Added more description.

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 octoios
 =======
 
-Scripts for automatic build, deploy and launch iOS apps on devices  
+Scripts for automatic build, deploy and launch iOS apps on connected Apple devices.
+
+It builds .app with `xcodebuild` command if -n flag is not used. Run `fruitstrap` to upload application to all connected devices. Then it uses `instruments` command to run applications.
+
+* Xcode should be installed.
+* Keychain should be accessible without password. To unlock keychain:
+
+        $ secure unlock -p <password> {$HOME}/Library/Keychains/login.keychain
+
+* Valid provisioning profile and signing certificate should be installed. [Apple developers center](https://developer.apple.com/ "Apple developers center")
+
+Usage:
+
+    $ ./octoios.rb [options]
+
+    Options:
+      -s, --schema SCHEMA              Application schema to use with build command
+      -i, --bundle_id BUNDLE_ID        Bundle identifier of application
+      -b, --build_dir BUILD_DIR        Builds directory (default: {current_folder}/builds)
+      -r, --src_dir SRC_DIR            Project source folder (where .xcodeproj file is located)
+      -n, --no_build                   Don't build project, .app or .ipa app should exists in a BUILD_DIR
+      -h, --help                       Shows help message
+
+This script uses:
+
+* [Fruitstrap](https://github.com/igorsokolov/fruitstrap)
+* [Transporter chief](http://gamua.com/blog/2012/03/how-to-deploy-ios-apps-to-the-iphone-via-the-command-line/)
+
+TODO:
+
+* Run the script as a daemon (add `-d` option)
+* Handle USR1 signal to install and start App ( respect `--no_build` option )
+* Support optional config file for options (YAML)
+* Add more todos :)
--- octoios.rb
+++ octoios.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/ruby
 require 'optparse'
-require 'ostruct'
 
-class Processes
+class Octoios
 
   def initialize(options = {})
     @processes = {}
@@ -10,15 +9,13 @@ class Processes
     @ids = []
     @root_dir = File.expand_path(File.dirname(__FILE__))
 
+    $0 = "octoios-master"
+
+
     trap('TERM') { shutdown }
     trap('INT')  { shutdown }
     trap('QUIT') { shutdown }
     delete_zombie_processes
-
-    @ids = `./fruitstrap -q -t 1 list-devices`.split("\n")
-    abort 'No devices connected' if @ids.empty?
-    puts "Connected devices (#{@ids.count}):"
-    @ids.each{|_| puts _}
 
     puts "Build project"
     @options[:schema] = options[:schema]
@@ -28,6 +25,12 @@ class Processes
     system("cd #{@options[:src_dir]} && \
       xcodebuild -scheme #{@options[:schema]} -sdk iphoneos build \
       CONFIGURATION_BUILD_DIR='#{@options[:build_dir]}'") unless options[:no_build]
+
+    @ids = `./fruitstrap -q -t 1 list-devices`.split("\n")
+    abort 'No devices connected' if @ids.empty?
+    puts "Connected devices (#{@ids.count}):"
+    @ids.each{|_| puts _}
+
   end
 
   def start
@@ -43,7 +46,7 @@ class Processes
           Process.detach @child
         else
           @processes = {}
-          $0 = "build-process:#{id}"
+          $0 = "octoios-process:#{id}"
 
           # "Uninstall previous version"
           system("ruby transporter_chief.rb -v -d #{id} #{@options[:bundle_id]}")
@@ -98,7 +101,7 @@ class Processes
   end
 
   def process_pids
-    `ps -A -o pid,command | grep 'build-proces[s]'`.split("\n").map do |line|
+    `ps -A -o pid,command | grep 'octoios-proces[s]'`.split("\n").map do |line|
       line.split(' ')[0]
     end
   end
@@ -111,27 +114,27 @@ option_parser = OptionParser.new do |opts|
   opts.separator ""
   opts.separator "Options:"
 
-  opts.on('-s', '--schema SCHEMA', 'Build schema') do |schema|
+  opts.on('-s', '--schema SCHEMA', 'Application schema to use with build') do |schema|
     options[:schema] = schema
   end
 
-  opts.on('-i', '--bundle_id BUNDLE_ID', 'Bundle identifier of project') do |bundle_id|
+  opts.on('-i', '--bundle_id BUNDLE_ID', 'Bundle identifier of application') do |bundle_id|
     options[:bundle_id] = bundle_id
   end
 
-  opts.on('-b', '--build_dir BUILD_DIR', 'Builds directory') do |build_dir|
+  opts.on('-b', '--build_dir BUILD_DIR', 'Builds directory (default: {current_folder}/builds)') do |build_dir|
     options[:build_dir] = build_dir
   end
 
-  opts.on('-r', '--src_dir SRC_DIR', 'Source directory') do |src_dir|
+  opts.on('-r', '--src_dir SRC_DIR', 'Project source folder (where .xcodeproj file is located)') do |src_dir|
     options[:src_dir] = src_dir
   end
 
-  opts.on('-n', '--no_build', 'Skip build project path') do
+  opts.on('-n', '--no_build', 'Don\'t build project, .app or .ipa file should exists in BUILD_DIR') do
     options[:no_build] = true
   end
 
-  opts.on_tail('-h', '--help', 'Show this message') do
+  opts.on_tail('-h', '--help', 'Show help message') do
     puts opts
     exit
   end
@@ -144,4 +147,4 @@ else
   option_parser.parse!
 end
 
-Processes.new(options).start
+Octoios.new(options).start
